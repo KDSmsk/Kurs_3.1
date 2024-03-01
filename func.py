0ccb808881e9dc0659ce4bebda0be31c5418e05c
KDSmsk <157491617+KDSmsk@users.noreply.github.com>
2024-03-01 14:59:29 +0300

    Update func.py
    name

--- func.py
+++ func.py
@@ -4,11 +4,11 @@ import datetime
 from transction_obj import transaction
 
 
-def load_json_from_file(path1, file_name):
+def load_json_from_file(path_of_opration, file_name):
     """
     Функция возвращает объект по строкам из json файла
     """
-    file_ID = os.path.join(path1, file_name)  # Считываем файл с диска
+    file_ID = os.path.join(path_of_opration, file_name)  # Считываем файл с диска
     with open(file_ID, 'r', encoding='utf-8') as file_JS:  # Читаем файл построчно
         return json.load(file_JS)
 
